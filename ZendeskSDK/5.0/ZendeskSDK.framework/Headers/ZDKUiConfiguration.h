23ba371bd1009a0857eb457ccc8bb4f4014b7023
Ronan Mchugh <rmchugh@zendesk.com>
2019-03-28 18:19:17 +0000

    Release 2.3.1

---
+++ ZendeskSDK/5.0/ZendeskSDK.framework/Headers/ZDKUiConfiguration.h
@@ -0,0 +1,29 @@
+/*
+ *
+ *  ZDKUiConfiguration.h
+ *  ZendeskSDK
+ *
+ *  Created by Zendesk on 12/06/18.
+ *
+ *  Copyright (c) 2018 Zendesk. All rights reserved.
+ *
+ *  By downloading or using the Zendesk Mobile SDK, You agree to the Zendesk Master
+ *  Subscription Agreement https://www.zendesk.com/company/customers-partners/#master-subscription-agreement and Application Developer and API License
+ *  Agreement https://www.zendesk.com/company/customers-partners/#application-developer-api-license-agreement and
+ *  acknowledge that such terms govern Your use of and access to the Mobile SDK.
+ *
+ */
+
+#ifndef ZDKUiConfiguration_h
+#define ZDKUiConfiguration_h
+
+/**
+ * Configuration for a screen of the SDK
+ */
+@protocol ZDKUiConfiguration <NSObject>
+
+- (instancetype) init;
+
+@end
+
+#endif /* ZDKUiConfiguration_h */
